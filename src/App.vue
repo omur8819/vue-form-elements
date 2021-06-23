0eae6d1df2a96b2ac05adf50a82fc7f05414db14
omurbozdag <omur8819@gmail.com>
2021-06-23 17:24:28 +0300

    feat: form completed

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="container">
-    <h3>Form Verileriyle Çalışmak</h3>
+    <h3>Working with Form Data</h3>
     <hr>
     <div class="row">
       <div class="col-md-6">
         <div class="panel panel-warning">
           <div class="panel-heading">
-            <h4>Başvuru Formu</h4>
+            <h4>Application Form</h4>
           </div>
           <div class="panel-body">
             <form>
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group">
-                    <label for="email">Kullanıcı Adı</label>
-                    <input type="text" id="username" class="form-control" v-model="userData.username">
+                    <label for="email">Username</label>
+                    <input type="text"
+                          id="username" 
+                          class="form-control" 
+                          :value="userData.username" 
+                          @input="userData.username = $event.target.value">
                   </div>
                   <div class="form-group">
-                    <label for="password">Şifre</label>
+                    <label for="password">Password</label>
                     <input type="password" id="password" class="form-control" v-model.lazy.trim="userData.password">
                   </div>
                   <div class="form-group">
-                    <label for="age">Yaş</label>
+                    <label for="age">Age</label>
                     <input type="number" id="age" class="form-control" v-model.number.trim="userData.age">
                   </div>
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-12 form-group">
-                  <label for="message">Açıklama</label><br>
+                  <label for="message">Comments</label><br>
                   <textarea id="message" rows="3" class="form-control" v-model="userData.message"></textarea>
                 </div>
               </div>
@@ -36,10 +40,16 @@
                 <div class="col-md-12">
                   <div class="form-group">
                     <label>
-                      <input type="checkbox" value="yazilim"> Yazılım
+                      <input type="checkbox" value="software" v-model="userData.interests"> Software
                     </label>
                     <label>
-                      <input type="checkbox" value="donanim"> Donanım
+                      <input type="checkbox" value="hardware" v-model="userData.interests"> Hardware
+                    </label>
+                    <label>
+                      <input type="checkbox" value="front-end" v-model="userData.interests"> Front-End
+                    </label>
+                    <label>
+                      <input type="checkbox" value="back-end" v-model="userData.interests"> Back-End
                     </label>
                   </div>
 
@@ -48,26 +58,34 @@
               <div class="row">
                 <div class="col-md-12 form-group">
                   <label>
-                    <input type="radio" value="erkek"> Erkek
+                    <input v-model="userData.gender" type="radio" value="male"> Male
                   </label>
                   <label>
-                    <input type="radio" value="kadin"> Kadın
+                    <input v-model="userData.gender" type="radio" value="female"> Female
                   </label>
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-12 from-group">
-                  <label>Şehir</label>
-                  <select class="form-control">
-                    <option></option>
+                  <label>City</label>
+                  <select class="form-control" v-model="userData.selectedCity">
+                    <option :selected="city == 'London'" v-for="city in userData.cities" v-bind:key="city"> {{ city }} </option>
                   </select>
                 </div>
               </div>
               <hr>
+
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <app-switch v-model="switched"></app-switch>
+                </div>
+              </div>
+
               <div class="row">
                 <div class="col-md-12">
                   <button
-                    class="btn btn-primary">Gönder!
+                    @click.prevent="submit"
+                    class="btn btn-primary">Send!
                   </button>
                 </div>
               </div>
@@ -75,23 +93,23 @@
           </div>
         </div>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" v-if="isSubmitted">
           <div class="panel panel-info">
             <div class="panel-heading">
-              <h4>Form Verileri</h4>
+              <h4>Form Data</h4>
             </div>
             <div class="panel-body">
-              <p>Kullanıcı Adı: {{ userData.username }}</p>
-              <p>Şifre:  {{ userData.password }}</p>
-              <p>Yaş: {{ userData.age }}</p>
-              <p style="white-space: pre;">Açıklama: {{ userData.message }} </p>
-              <p><strong>İlgi Alanları</strong></p>
+              <p>Username: {{ userData.username }}</p>
+              <p>Password:  {{ userData.password }}</p>
+              <p>Age: {{ userData.age }}</p>
+              <p style="white-space: pre;">Comments: {{ userData.message }} </p>
+              <p><strong>Interests</strong></p>
               <ul>
-                <li></li>
+                <li v-for="item in userData.interests" v-bind:key="item"> {{ item }} </li>
               </ul>
-              <p>Cinsiyet:</p>
-              <p>Şehir:</p>
-              <p>Toggle:</p>
+              <p>Gender: {{userData.gender}}</p>
+              <p>City: {{ userData.selectedCity }} </p>
+              <p>Toggle:  {{ switched }}</p>
             </div>
           </div>
       </div>
@@ -101,6 +119,7 @@
 </template>
 
 <script>
+  import Switch from './Switch.vue';
   export default {
     data(){
       return{
@@ -109,9 +128,23 @@
           password: '',
           age: 30,
           message: '',
-        }
+          interests: [],
+          gender: '',
+          cities: ["Istanbul", "London", "Paris", "New York", "Athens"],
+          selectedCity: '',
+        },
+        switched: true,
+        isSubmitted: false,
       }
-    }
+    },
+    components: {
+      appSwitch: Switch,
+    },
+    methods: {
+      submit(){
+        this.isSubmitted = true;
+      }
+    },
   }
 </script>
 
---
+++ src/Switch.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div id="on" @click="switched(true)" :class="{ active: value }">Open</div>
+        <div id="off" @click="switched(false)" :class="{ active: !value }">Close</div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ["value"],
+        methods: {
+            switched(isOn){
+                this.$emit("input", isOn)
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+  #on, #off {
+    width: 50px;
+    height: 20px;
+    background-color: #eee;
+    padding: 2px;
+    display: inline-block;
+    margin: 10px -2px;
+    box-sizing: content-box;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  #on:hover,#on.active {
+    background-color: lightgreen;
+  }
+
+  #off:hover,#off.active {
+    background-color: red;
+  }
+
+</style>
+
